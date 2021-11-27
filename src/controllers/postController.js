0169e900ed9127d723dad1a582de02806b397a65
luanvn2203 <luanvnse63360@gmail.com>
2021-11-28 03:25:14 +0700

    fix bug

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,7 +1,7 @@
 import { USER_TRANS_ERROR } from "../../lang/vi"
 import { CLIENT_ERROR_STATUS, SUCCESSFUL_STATUS } from "../configs/httpStatusCode"
 import postService from '../services/post.service'
-import { handleError } from '../utils/Helper'
+import { handleError2 } from '../utils/Helper'
 
 /**
  * create new post
@@ -16,18 +16,19 @@ async function createNewPost(req, res) {
             error: USER_TRANS_ERROR.PARAMS_INVALID
         })
     }
-    handleError(req, res)
-    const post = {
-        authorId: req.signInUser.email,
-        title: req.body.params.title,
-        metaTitle: req.body.params.metaTitle || null,
-        avatar: req.body.params.avatar || 'https://saigongiftbox.com/wp-content/uploads/2021/03/11-20-780x470.jpg',
-        slug: req.body.params.slug || null,
-        summary: req.body.params.summary || null,
-        isPublished: req.body.params.isPublished,
-        content: req.body.params.content,
-    }
+
     try {
+        handleError2(req, res)
+        const post = {
+            authorId: req.signInUser.email,
+            title: req.body.params.title,
+            metaTitle: req.body.params.metaTitle || null,
+            avatar: req.body.params.avatar || 'https://saigongiftbox.com/wp-content/uploads/2021/03/11-20-780x470.jpg',
+            slug: req.body.params.slug || null,
+            summary: req.body.params.summary || null,
+            isPublished: req.body.params.isPublished,
+            content: req.body.params.content,
+        }
         const result = await postService.createNewPost(post)
         return res.status(SUCCESSFUL_STATUS.CREATED).json({
             message: result
@@ -46,18 +47,19 @@ async function createNewPost(req, res) {
  * @returns response
  */
 async function updatePost(req, res) {
-    handleError(req, res)
 
-    const post = {
-        title: req.body.params.title,
-        metaTitle: req.body.params.metaTitle,
-        avatar: req.body.params.avatar,
-        slug: req.body.params.slug,
-        summary: req.body.params.summary,
-        isPublished: req.body.params.isPublished,
-        content: req.body.params.content,
-    }
     try {
+        handleError2(req, res)
+
+        const post = {
+            title: req.body.params.title,
+            metaTitle: req.body.params.metaTitle,
+            avatar: req.body.params.avatar,
+            slug: req.body.params.slug,
+            summary: req.body.params.summary,
+            isPublished: req.body.params.isPublished,
+            content: req.body.params.content,
+        }
         const result = await postService.updatePost(post, req.body.params.id, req.signInUser.email)
         return res.status(SUCCESSFUL_STATUS.OK).json({
             message: result
